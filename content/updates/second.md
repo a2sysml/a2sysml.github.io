5192428b11efb5eec45ab97896e35d5dafced517
iJustErikk <iJustErikk@users.noreply.github.com>
2023-12-17 00:02:10 +0000

    deploy: 289424849152db5d2f8b559a0d637b060f3a27f5

--- content/updates/second.md
+++
@@ -1,6 +0,0 @@
-+++
-title = "Our second update"
-date = 2019-11-28
-+++
-
-This is our second update.
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
       backgroundImage: {
         'small-bg': "url('/a2huronportrait.webp')",
         'large-bg': "url('/a2huron.jpg')"
+      },
+      colors: {
+        'primary': '#0b9a6d',
+        'secondary': '#18453b',
+        'tertiary': '#ceebe2',
+        'grey1': '#222',
+        'grey2': '#333',
+        'button-primary': '#0787ad',
+        'button-secondary': '#0b9a6d'
       }
     }
   },
